18743344a92ea6105ecb7e6bc79d7e7482eca729
Ahmad Moemen <37182876+AhmadMoemen@users.noreply.github.com>
2022-07-27 08:39:01 +0200

    `erofs`: Add oplus rebuild script.
    What it does is batch converting oplus images from erofs to ext4.
    Then it moves the ext4 images to the output (ext4) folder, then runs the merger files to save time.

---
+++ oplus-rebuild.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "[INFO] Cleaning up existing build residue"
+rm -rf ext4/*.img
+PARTITIONS="my_product my_engineering my_company my_carrier my_region my_heytap my_stock my_preload my_bigball my_manifest system vendor product system_ext"
+echo "Batch converting erofs images..."
+echo
+for part in $PARTITIONS
+do
+	if [ -f "$part.img" ]; then
+	echo "[INFO] Converting $part.img to erofs"
+	./erofs.sh $part.img $part
+	mv *-ext4.img ext4/
+	fi
+done
+[ $(du -sm $MERGEDIR | awk '{printf $1}') -le 20 ] cp product.img ext4/
+cd ext4
+for part in $PARTITIONS
+do
+	mv $part-ext4.img $part.img
+done
+echo "[INFO] Merging partitions into product.img"
+./product-merge.sh
+
+echo "[INFO] Merging partitions into system.img"
+./oplus-merge.sh
+
+echo "[INFO] Rebuild oplus images & merge done."
